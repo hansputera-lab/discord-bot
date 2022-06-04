8032013a93a1fcd361ae79da227038f7534bc71a
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-04 12:08:07 +0700

    fix: fixed prettier configuration
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ bot.js
@@ -0,0 +1,22 @@
+import { CommandClient } from 'eris';
+
+/**
+ * Create bot instance.
+ * @param {string} token Bot token.
+ * @param {Object} options Eris options.
+ * @param {Object} commandOptions Eris command options.
+ * @return {Client}
+ */
+export const createBot = (token, options, commandOptions) =>
+	new CommandClient(
+		'Bot '.concat(token),
+		{
+			...options,
+			defaultImageFormat: 'png',
+		},
+		{
+			...commandOptions,
+			owner: 'hansputera',
+			description: "A discord bot to serve hansputera's lab",
+		},
+	);
--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import 'dotenv/config';
 
-console.log('Hello world');
+import assert from 'node:assert';
+
+import { CommandClient } from 'eris';
+import { createBot } from './bot.js';
+
+assert.ok(typeof process.env.TOKEN === 'string');
+const bot = createBot(process.env.TOKEN);
+
+assert.ok(bot instanceof CommandClient);
+
+bot.on('ready', () => {
+	bot.editStatus('dnd', {
+		name: 'w/ filo!',
+		type: 0,
+	});
+
+	console.log(bot.user.username, 'ready to serve!');
+});
+
+bot.connect();
